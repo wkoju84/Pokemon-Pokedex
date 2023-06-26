4471e047bb79204492bb5a8e638b44c05cd90776
william Silva <wkoju84@gmail.com>
2023-06-26 12:43:11 -0300

    Concluindo Pokedex

---
+++ assets/js/poke-api.js
@@ -0,0 +1,38 @@
+
+
+const pokeApi = {}
+
+function convertPokeApiDetaiToPokemon(pokeDetail){
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.id
+    pokemon.name = pokeDetail.name
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+            .then((response) => response.json())
+            .then(convertPokeApiDetaiToPokemon)
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 10) => {
+    // Requisição assíncrona
+   const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+    return fetch(url)
+            .then((response) => response.json())//Converte para json
+            .then((jsonBody) => jsonBody.results)//Recebe convertido
+            .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+            .then((detailRequests) => Promise.all(detailRequests))
+            .then((pokemonsDetails) => pokemonsDetails)
+            .catch((error) => console.error(error));// Tratamento de erro
+}
